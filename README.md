b5fd6ddef591d072b31fca99800d7f51d14dc041
naeem <naeem@DESKTOP-VALOQAE.localdomain>
2026-08-15 07:39:47 +0000

    create new files and some changes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # demo-naeem1
 This repository is a demo repo for prectice git and github.
+
+# role
+a devops engineer 
+
+# tools
+linux,docker,kubernates,networking,aws
---
+++ naeem.java
@@ -0,0 +1,13 @@
+public class naeem{
+    public static void main(String args[]){
+        int x=200, y=50, z=100;
+        if(x>y && y>z){
+            System.out.println("hello");
+        }
+        if(z>y && z<x){
+            System.out.println("naeem");
+        }
+        if((y+200) < x && (y+150) <z){
+            System.out.println("hello java");}
+    }
+}
